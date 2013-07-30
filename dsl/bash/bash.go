d55f614b4c91171619e5430a3bcae6b2c49c0e9c
Sebastian Morr <sebastian@morr.cc>
2013-07-30 18:20:17 +0200

    Organized lessons and "dsl" module

---
+++ dsl/bash/bash.go
@@ -0,0 +1,27 @@
+package bash
+
+import (
+	"fmt"
+	"strconv"
+	"morr.cc/nutsh.git/dsl"
+)
+
+func QueryReturn(query string) int {
+	dsl.Query(query)
+	output := dsl.Query("echo $?")
+	value, _ := strconv.Atoi(output[0:len(output)-2])
+	return value
+}
+
+func Test(expression string) bool {
+	return QueryReturn("test "+expression) == 0
+}
+
+func Execute(command string) {
+	output := dsl.Query(command)
+	valuestring := dsl.Query("echo $?")
+	value, _ := strconv.Atoi(valuestring[0:len(valuestring)-2])
+	if value != 0 {
+		panic(fmt.Sprintf("executing `%s` failed: %s", command, output))
+	}
+}
---
+++ dsl/dsl.go
@@ -0,0 +1,54 @@
+package dsl
+
+import (
+	"fmt"
+	"regexp"
+	"morr.cc/nutsh.git/cli"
+)
+
+var (
+	cmdline cli.CLI
+	lastCommand, lastOutput string
+	wasInteractive bool
+	didOutput bool
+)
+
+func Spawn(target string) {
+	cmdline = cli.Spawn(target)
+}
+
+func Query(query string) string {
+	return cmdline.Query(query)
+}
+
+func QueryOutput(query string, expression string) bool {
+	output := cmdline.Query(query)
+	return regexp.MustCompile(expression).MatchString(output)
+}
+
+func Say(text string) {
+	text = regexp.MustCompile("`([^`]+)`").ReplaceAllString(text, "[32m$1[36m")
+	fmt.Printf("[36m\n\n    %s\n\n[0m", text)
+}
+
+func Command(expression string) bool {
+	return regexp.MustCompile(expression).MatchString(lastCommand)
+}
+
+func OutputMatch(expression string) bool {
+	return regexp.MustCompile(expression).MatchString(lastOutput)
+}
+
+func Output() {
+	if !wasInteractive && !didOutput {
+		fmt.Print(lastOutput)
+		didOutput = true
+	}
+}
+
+func Prompt() bool {
+	didOutput = false
+	lastCommand = cmdline.ReadCommand()
+	lastOutput, wasInteractive = cmdline.ReadOutput()
+	return true
+}
