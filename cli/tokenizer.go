a7a6d2790c7227209cb4490d863988c2a5c4de5d
Sebastian Morr <sebastian@morr.cc>
2013-09-12 00:19:43 +0200

    Timing problems

--- cli/tokenizer.go
+++ cli/tokenizer.go
@@ -117,7 +117,7 @@ func tokenize(input <-chan rune, tokens chan<- token, runes chan<- rune, state *
 					tokens <- token{finalCommandType, buffer[0:len(buffer)-1] + "\n"}
 					*state++
 					interactive = false
-					timer.Reset(1 * time.Millisecond)
+					timer.Reset(100 * time.Millisecond)
 				}
 			}
 			buffer = ""
